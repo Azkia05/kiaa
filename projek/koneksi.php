c583cc1169aadb1157047f4ee7a9194d6bdeb1ad
Azkia05 <azkiakafkanafisyaa@gmail.com>
2026-05-26 12:34:08 +0700

    upload projek

--- projek/koneksi.php
+++ projek/koneksi.php
@@ -1,7 +1,12 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "2526_26db");
-if(!$conn){
+$host = "localhost";
+$user = "2526_01";
+$pass = "12345678";
+$db = "2526_26db";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-session_start();
 ?>
